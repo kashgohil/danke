7e9b5100142112b17c1e6ed0d0a4cb81110082b4
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-31 02:10:57 +0530

    [chore] utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function generateToken(): string {
-  return Math.random().toString(36).substring(2) + Date.now().toString(36);
+  return twMerge(clsx(inputs))
 }
